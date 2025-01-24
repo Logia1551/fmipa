a7a08fcf5c146f10b33ed9d17573cff9d1e65e3c
Logia <firaal851@gmail.com>
2025-01-25 00:41:37 +0800

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Sidebar from './components/Sidebar'
 import Header from './components/header'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,12 +11,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex`}>
-        <Sidebar />
-        <div className="flex-1 flex flex-col">
-          <Header />
-          <main className="flex-1">{children}</main>
-        </div>
+      <body className={`${inter.className} flex flex-col`}>
+        <Header />
+        <main className="flex-1">{children}</main>
       </body>
     </html>
   )
